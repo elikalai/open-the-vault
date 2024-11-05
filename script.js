bdb821af1b3b2ba2062eca00d436352e4759efd3
Elijah Irvine <elikalaixiv@gmail.com>
2024-11-05 10:11:50 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+/* the code is 10-40-39
+1. define equasion that equals 10
+2. define equasion that equals 40
+3. define equasion that equals 39
+4. assign sums to combination
+5. display alert of combination 
+*/
+
+//initializing welcome message 
+let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+//equasion that equals 10
+const num1 = 5 * 2
+
+//equasion that equals 40
+const num2 = 20 * 2
+
+//equasion that equals 39
+const num3 = 30 + 9
+
+//assign sums to combination
+let combination = num1 + "_" + num2 + "_" + num3
+
+//display alert of combination
+alert(`${message} to the vault is ${num1}-${num2}-${num3}`);
+
